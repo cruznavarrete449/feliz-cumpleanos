9592c485362893ce10c730396913b385b7aad460
Cesar Navarrete <cruznavarrete449@gmail.com>
2026-06-04 22:38:14 -0600

    Version 4

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function abrirSorpresa(){
 
+    let colores = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "pink",
+    "cyan"
+];
+
+let indice = 0;
+
+function cambiarColor(){
+
+    let boton = document.getElementById("botonColor");
+
+    let color =
+    "#" + Math.floor(Math.random()*16777215).toString(16);
+
+    boton.style.backgroundColor = color;
+
+}
+
     document.getElementById("mensaje").style.display = "block";
 
     document.getElementById("mensaje").style.animation = "aparecer 1s";
