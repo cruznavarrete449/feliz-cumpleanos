5f2a03c8702b5b98157558136421fabde41a6c8c
Cesar Navarrete <cruznavarrete449@gmail.com>
2026-06-04 21:54:56 -0600

    Version inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirSorpresa(){
+
+document.getElementById("mensaje").style.display="block";
+
+document.getElementById("mensaje").style.animation="aparecer 1s";
+
+document.body.style.background="#fff8dc";
+
+}
