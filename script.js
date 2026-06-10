65bb923d8811fd57be08277bf4ad6a549196dba3
Cesar Navarrete <cruznavarrete449@gmail.com>
2026-06-10 10:04:44 -0600

    Version 5

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function abrirSorpresa(){
 
     document.getElementById("musica").play();
 
+    crearGlobos();    
+
 }
 
 let colores = [
@@ -30,6 +32,46 @@ function cambiarColor(){
 
     if(indice >= colores.length){
         indice = 0;
+    }
+
+}
+
+
+function crearGlobos(){
+
+    const contenedor = document.getElementById("globos");
+
+    const colores = [
+        "red",
+        "blue",
+        "green",
+        "yellow",
+        "purple",
+        "orange",
+        "pink",
+        "cyan"
+    ];
+
+    for(let i = 0; i < 30; i++){
+
+        let globo = document.createElement("div");
+
+        globo.classList.add("globo");
+
+        globo.style.left = Math.random()*100 + "%";
+
+        globo.style.backgroundColor =
+        colores[Math.floor(Math.random()*colores.length)];
+
+        globo.style.animationDuration =
+        (Math.random()*3 + 4) + "s";
+
+        contenedor.appendChild(globo);
+
+        setTimeout(()=>{
+            globo.remove();
+        },7000);
+
     }
 
 }
